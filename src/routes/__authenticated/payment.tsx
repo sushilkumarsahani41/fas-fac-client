0b74628b53fe0b43b43cb7e47e12bd8b28b8e3bb
sushilkumar sahani <dev.sahanisushilkumar@gmail.com>
2025-01-10 06:23:40 +0530

    Fas-Fac Payment Integration

--- src/routes/__authenticated/payment.tsx
+++ src/routes/__authenticated/payment.tsx
@@ -1,3 +1,22 @@
 import PayUCheckout from "@/payment.tsx";
+import AppError from "@/components/shared/app-error.tsx";
+import CommonLayout from "@/layouts/common-layout.tsx";
+import Navbar from "@/components/shared/navbar.tsx";
+import VerifyPayment from "@/verifyPayment.tsx";
 
+export function Component() {
+    return (
+        <CommonLayout>
+            <div className="m-auto max-w-7xl px-4">
+                <Navbar />
+                <div className="flex flex-col justify-center items-center h-full py-20">
+                    <PayUCheckout/>
+                </div>
+            </div>
+        </CommonLayout>
+    )
+}
 
+export function ErrorBoundary() {
+    return <AppError/>
+}
--- src/routes/__authenticated/verify-payment.tsx
+++ src/routes/__authenticated/verify-payment.tsx
@@ -1,11 +1,18 @@
 import VerifyPayment from "@/verifyPayment.tsx";
+import CommonLayout from "@/layouts/common-layout.tsx";
+import Navbar from "@/components/shared/navbar.tsx";
 
 
 export function Component() {
 
     return (
-        <div>
-            <VerifyPayment />
-        </div>
-    )
+        <CommonLayout>
+            <div className="m-auto max-w-7xl px-4">
+                <Navbar />
+                <div className="flex flex-col justify-center items-center h-full py-20">
+                    <VerifyPayment/>
+                </div>
+            </div>
+        </CommonLayout>
+)
 }
